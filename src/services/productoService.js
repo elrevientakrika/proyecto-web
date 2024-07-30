706396f0abd704271d5b62c58354390851a2be32
Adonis <Adonis@sys.leca39.cl>
2024-07-30 11:03:50 -0400

    primera version del proyecto web node

---
+++ src/services/productoService.js
@@ -0,0 +1,30 @@
+import config from '../config/knexConfig.js'
+import knex from 'knex'
+export default class ProductService{
+    constructor(){
+        this.knex = knex(config)
+    }
+
+    getProductos = async() =>{
+        const productos = await this.knex.from('productos').select('*')
+        return Object.values(JSON.parse(JSON.stringify(productos)))
+    }
+
+    addProducto = async(producto) => {
+        const respuesta = this.knex('productos').insert(producto)
+        return respuesta
+    }
+
+    updateProductoById = async(id,producto) => {
+       return this.knex('productos').where({producto_id:id}).update(producto)
+    }
+
+    showProductoById = async(id) =>{
+        return this.knex('productos').where({producto_id:id}).first()
+    }
+
+    deleteProductoById = async(id) => {
+        return this.knex('productos').where({producto_id:id}).del()
+    }
+}
+
